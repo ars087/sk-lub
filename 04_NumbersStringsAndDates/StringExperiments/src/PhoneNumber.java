cb8c77c2af64f6292c0a2ef273e4e4bc2bee7398
Arsen Azaryan <ars5885@gmail.com>
2020-05-15 02:57:28 +0400

    Module4[homework4.5.4-correction-1]"

--- 04_NumbersStringsAndDates/StringExperiments/src/PhoneNumber.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/PhoneNumber.java
@@ -3,37 +3,33 @@ import java.util.Scanner;
 
   public class PhoneNumber {
 
+
         public static void main(String[] args) {
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println(" Напишите вашу Фамилию, Имя, Отчество!");
 
-            int rightLong;
+            final String REG_FULLNAME = "^[7|8]\\d{10}"; // в этой строке впишите регулярку
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите ваш номер телефона!");
+
+
+            String   phoneNumber   = scanner.nextLine();
+            //String countText = phoneNumber.replaceAll("[\\D]","");
+
             boolean numberOne = false ;
 
-            String phoneNumber;
+        for ( ; ; ){
 
-            while ( !numberOne ){
-                phoneNumber   = scanner.nextLine();
+                if (phoneNumber.matches(REG_FULLNAME) ){
 
-                String countText = phoneNumber.replaceAll("[A-Z a-z.!@'|><`~^%#&?/,()*+\\-]","");
-
-                char firstNumber = countText.charAt(0);
-                rightLong = countText.length();
-
-                if (rightLong == 11 && firstNumber == '7' || firstNumber == '8' ){
-
-                    System.out.println("Номер введен верно!");
-                    System.out.println(countText);
-
-                    numberOne =true;
-
-                }else {
-                    System.out.println(countText.length());
+                    System.out.println("Номер введен верно");
+                    phoneNumber   = scanner.nextLine();
+                }
+                else {
+                    //System.out.println(countText.length());
                     System.out.println("Номер телефона введен с ошибкой!!Количество цифр не должно быть больше 11 или меньше 11");
                     System.out.println("Если вы из России первая цифра вашего телефонного номера должна начинаться с цифры 7, либо 8");
-
-
+                    phoneNumber   = scanner.nextLine();
                 }
             }
         }
