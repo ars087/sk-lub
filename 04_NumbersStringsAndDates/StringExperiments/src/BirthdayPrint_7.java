9f0dbca7737c64a06792420e826547c8e98ca2f2
Arsen Azaryan <ars5885@gmail.com>
2020-06-03 17:19:03 +0400

    Module4[homework4.6.1-version-7]"

---
+++ 04_NumbersStringsAndDates/StringExperiments/src/BirthdayPrint_7.java
@@ -0,0 +1,57 @@
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+class test_6 {
+
+    public static void main(String[] args) throws ParseException {
+
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd");
+        dateFormat.setLenient(false);
+        Calendar birthday = Calendar.getInstance();
+        for (; ;) {
+            String input = new Scanner(System.in).nextLine();
+
+            try {
+                birthday.setTime(dateFormat.parse(input));
+
+            } catch (ParseException e) {
+
+                System.out.println("вы ввели неверный формат даты! правильный формат - 1990.12.21");
+                e.getMessage();
+                return;
+            }
+
+            printAllBirthday(birthday);
+        }
+    }
+    public static void printAllBirthday(Calendar birthdayOne)  {
+        DateFormat dateFormat = new SimpleDateFormat("EEEE, d MMMM yyyy");
+        Calendar today = Calendar.getInstance();
+
+        System.out.println(dateFormat.format(birthdayOne.getTime()));
+
+        while ( today.after(birthdayOne)) {
+
+            if( birthdayOne.get(Calendar.MONTH ) == Calendar.FEBRUARY && birthdayOne.get(Calendar.DATE)== 29) {
+
+                birthdayOne.roll(Calendar.YEAR, 4);
+
+                if (today.after(birthdayOne)) {
+                    System.out.println(dateFormat.format(birthdayOne.getTime()));
+                }
+
+            } else {
+
+                birthdayOne.roll(Calendar.YEAR, 1);
+
+                if (today.after(birthdayOne)) {
+                    System.out.println(dateFormat.format(birthdayOne.getTime()));
+                }
+            }
+        }
+    }
+}
