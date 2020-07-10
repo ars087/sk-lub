197e788fdd3bc4809ad296c0803f5351dee86c8f
Arsen Azaryan <ars5885@gmail.com>
2020-07-10 15:10:46 +0400

    Homework.Module 5,assignments[5.4]/corrections No.1

--- 05_ArraysAndCollections/src/Task_5_4_1.java
+++ 05_ArraysAndCollections/src/Task_5_4_1.java
@@ -11,77 +11,110 @@ public class Task_5_4_1 {
   public static final String REG_LIST = "LIST\\s*+";
   public static final String REG_PHONE = "^[7|8]\\d{10}";
   public static final String REG_NAME = "[А-Яа-я]+";
-  public static final String REG_NAME_PHONE = "^[7|8]\\d{10}|[А-Яа-я]+";//"; //^[7|8]\\d{10}";
-
-  public static String name = "";
-  public static String phoneNumber = "";
-  public static String input = "";
 
   public static void main(String[] args) {
-
     System.out.println("Введите имя или телефонный номер");
     for (; ; ) {
-      if (name.length() > 0 && phoneNumber.length() > 0) {
-        list.put(phoneNumber, name);
-        System.out.println("Запись добавлена");
-        name = "";
-        phoneNumber = "";
-      }
-      input = scanner.nextLine();
-      comparison(input);
-      add(input);
-      addPhoneNumber(input);
-      printPhoneBook(input);
-      size();
-    }
-  }
 
-  public static void printPhoneBook(String phone) {
-    if (phone.matches(REG_LIST)) {
-      System.out.println("Длина листа" + list.size());
-      for (String key : list.keySet()) {
-        System.out.println(list.get(key) + "||" + key);
-      }
-      System.out.println(list.size());
-    }
-  }
+      String input = scanner.nextLine();
+      if (isPhone(input)) {
+        if (isContainsKey(input)) {
+          printContactByPhone(input);
+        } else {
+          String name = getInputName();
+          list.put(input, name);
+          System.out.println("Запись добавлена!");
+          size();
+          System.out.println("Введите имя или телефонный номер в формате 7/89270010000");
 
-  public static void add(String add) {
-    if (add.matches(REG_NAME)) {
-      name = add;
-      System.out.println("Имя добавлено");
-    }
-  }
-
-  public static void addPhoneNumber(String addPhoneNumder) {
-    if (addPhoneNumder.matches(REG_PHONE)) {
-      phoneNumber = addPhoneNumder;
-      System.out.println("Номер добавлен");
-    }
-  }
-
-  public static String comparison(String addPhone) {
-    if (addPhone.matches(REG_NAME_PHONE)) {
-      if (list.containsKey(addPhone)) {
-        System.out.println(list.get(addPhone) + " " + addPhone);
-        System.out.println("Данный  номер телефона уже существует! Измените запись");
-        input = "";
-      } else if (list.containsValue(addPhone)) {
-        for (Map.Entry entry : list.entrySet()) {
-          if (addPhone.equals(entry.getValue())) {
-            System.out.println(entry.getValue() + " " + entry.getKey());
-          }
         }
-        System.out.println("Данное имя уже существует!Измените запись");
-        input = "";
+      } else if (isName(input)) {
+        if (list.containsValue(input)) {
+          printContactByName(input);
+        } else {
+          String phoneNumber = getInputPhone();
+          list.put(phoneNumber, input);
+          System.out.println("Запись добавлена!");
+          size();
+          System.out.println("Введите имя или телефонный номер в формате 7/89270010000.");
+        }
+      } else if (input.matches(REG_LIST)) {
+        printPhoneBook();
+      } else {
+        System.out.println("Не верный ввод формата.");
       }
     }
-    return input;
+  }
+
+  public static boolean isContainsKey(String key) {
+    return list.containsKey(key);
+  }
+
+  public static boolean isName(String phoneNumber) {
+    return phoneNumber.matches(REG_NAME);
+  }
+
+  public static boolean isPhone(String phoneNumber) {
+    return phoneNumber.matches(REG_PHONE);
+  }
+
+  public static String getInputPhone() {
+    String phoneNumber;
+    for (; ; ) {
+      System.out.println("Запишите номер телефона.");
+      String name = new Scanner(System.in).nextLine();
+      if (name.matches(REG_PHONE)) {
+        if (isContainsKey(name)) {
+          System.out.println("Данный  номер телефона уже существует!Используйте другой номер");
+        } else {
+          phoneNumber = name;
+          break;
+        }
+      } else {
+        System.out.println("Номер телефона не соответствует формату ввода!");
+      }
+    }
+    return phoneNumber;
+  }
+
+  public static String getInputName() {
+    String namePhone;
+    for (; ; ) {
+      System.out.println("Запишите  имя контакта.");
+      String name = new Scanner(System.in).nextLine();
+      if (name.matches(REG_NAME)) {
+        namePhone = name;
+        break;
+      } else {
+        System.out.println("Имя не соответствует формату ввода!");
+      }
+    }
+    return namePhone;
+  }
+
+  public static void printPhoneBook() {
+    for (String key : list.keySet()) {
+      System.out.println("Имя контакта :" + " " + list.get(key) + " \t" + "Номер телефона :" + key);
+    }
+  }
+
+  public static void printContactByPhone(String phoneNumber) {
+    System.out.println(list.get(phoneNumber) + " " + phoneNumber);
+    System.out.println("Данный  номер телефона уже существует! Измените запись.");
+  }
+
+  public static void printContactByName(String name) {
+    for (Map.Entry entry : list.entrySet()) {
+      if (name.equals(entry.getValue())) {
+        System.out.println(entry.getValue() + " " + entry.getKey());
+      }
+    }
+    System.out.println("Данное имя уже существует!Измените запись");
   }
 
   public static void size() {
     if (list.size() > 0) {
-      System.out.println("Записей в телефонной книге" + list.size());
+      System.out.println("Записей в телефонной книге :" + list.size());
     }
   }
 }
