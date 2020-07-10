f81946bbb2cb498d18b1f2d0cbefb43f3ebfa260
Arsen Azaryan <ars5885@gmail.com>
2020-07-10 19:49:13 +0400

    Homework.Module 5,assignments[5.3]/corrections No.2

--- 05_ArraysAndCollections/src/Task_5_3_1.java
+++ 05_ArraysAndCollections/src/Task_5_3_1.java
@@ -5,35 +5,36 @@ public class Task_5_3_1 {
 
   public static final String REG_EMAIL = "\\b([\\dA-Za-z[_][-][!#$%&'*+/=?^_`{|}~]]([\\.])?)+\\b@\\b([\\da-z]([-])?)+\\b\\.[A-Za-z]+";
   private static final String REG_ADD = "^ADD\\s+" + REG_EMAIL + "$";
-  private static final String REG_LIST = "LIST";
+  private static final String REG_LIST = "LIST\\s*+";
   private static final String REG_CLEAR = "CLEAR\\s*+";
 
   private static TreeSet<String> list = new TreeSet();
 
   public static void main(String[] args) {
     for (; ; ) {
+      System.out.println("Добавьте E-mail в список. Пример : ADD hello@skillbox.ru");
       String input = new Scanner(System.in).nextLine();
-    /*  String[] addArray = input.split("\\s+", 2);
-      if (addArray.length > 1) {
-        String todoText = addArray[1];
-        addE_mail(todoText);
-      }*/
-      addE_mail(todoSplit(input));
-      listPrint(input);
-      clear(input);
-
-      if (list.size() > 0) {
-        System.out.println("Количество E-mail в списке :" + list.size());
-      } else {
-        System.out.println("Список пуст");
+      if (input.matches(REG_ADD)) {
+        addE_mail(todoSplit(input));
+      } else if (input.matches(REG_LIST)) {
+        listPrint();
+      } else if (input.matches(REG_CLEAR)) {
+        clear();
       }
     }
   }
 
   private static void addE_mail(String add) {
-    if (add.matches(REG_EMAIL)) {
-      list.add(add);
-      System.out.println("E-mail добавлен в список");
+    list.add(add);
+    System.out.println("E-mail добавлен в список");
+    size();
+  }
+
+  private static void size() {
+    if (list.size() > 0) {
+      System.out.println("Количество E-mail в списке :" + list.size());
+    } else {
+      System.out.println("Список пуст!");
     }
   }
 
@@ -46,20 +47,16 @@ public class Task_5_3_1 {
     return todoText;
   }
 
-  private static void listPrint(String listInput) {
-    if (listInput.matches(REG_LIST)) {
-      for (String list : list) {
-        System.out.println(list);
-      }
+  private static void listPrint() {
+    size();
+    for (String list : list) {
+      System.out.println(list);
     }
   }
 
-  private static void clear(String clear) {
-    if (clear.matches(REG_CLEAR)) {
-      if (list.size() > 0) {
-        list.clear();
-        System.out.println("Список очищен");
-      }
-    }
+  private static void clear() {
+    list.clear();
+    System.out.println("Список очищен");
+    size();
   }
 }
