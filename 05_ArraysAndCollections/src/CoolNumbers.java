d07ba1e2b6e3eb794f85399c00687eba7632d5cc
Arsen Azaryan <ars5885@gmail.com>
2021-03-28 21:31:32 +0400

    Homework.Module 5,assignments[5.5]/corrections No.1

--- 05_ArraysAndCollections/src/CoolNumbers.java
+++ 05_ArraysAndCollections/src/CoolNumbers.java
@@ -2,55 +2,63 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class CoolNumbers {
 
   public static final String[] lettersLicensePlates = {"А", "В", "Е", "К", "М", "Н", "О", "Р", "С",
       "Т", "У", "Х"};
-  public static final String REG_ADD_REGION = "[1-9]{1,3}";
+
   public static final String REG_NUMBER_CAR = "[А-Я](\\d{1,3})[А-Я]{1,3}\\d{1,3}";
 
-  public static final HashSet<String> COLECTION_NUMBER_HASH = new HashSet<>();
-  public static final TreeSet<String> COLECTION_NUMBER_SET = new TreeSet<>();
-  public static final ArrayList<String> COLECTION_NUMBER_LIST = new ArrayList<>();
-  public static final ArrayList<String> REGION_NUMBERS = new ArrayList<>();
-    public static void main(String[] args) {
+  public static final List<String> COLECTION_NUMBER_LIST = new ArrayList<>();
+  public static final Set<String> COLECTION_NUMBER_SET = new TreeSet<>();
+  public static final Set<String> COLECTION_NUMBER_HASH = new HashSet<>();
+
+  public static void main(String[] args) {
+
+    generateCoolNumbers();
+    System.out.println("База номеров сгенерирована!");
 
     for (; ; ) {
 
-      System.out.print("Введите  номер региона   либо команду!");
+      System.out.print("Введите регистрационный номер автомобиля, либо команду!");
       Scanner scanner = new Scanner(System.in);
       String valueInput = scanner.nextLine();
-
-      inputRegion(valueInput);
-     binarySearch(valueInput,COLECTION_NUMBER_SET);
-      //containsSearch(valueInput,COLECTION_NUMBER_LIST);
       printArray(valueInput);
+      if (valueInput.matches(REG_NUMBER_CAR)) {
 
-      if (!valueInput.matches(REG_ADD_REGION) && !valueInput.equals("PRINT") && !valueInput
+        searchInArrayList(valueInput, COLECTION_NUMBER_LIST);
+        searchInTreeSet(valueInput, COLECTION_NUMBER_SET);
+        searchInHashSet(valueInput, COLECTION_NUMBER_HASH);
+
+
+      } else if (!valueInput.equals("PRINT") && !valueInput
           .matches(REG_NUMBER_CAR)) {
 
         System.out.println("Команда введена в неверном формате");
       }
 
-    }////////////конец функции.
+    }
 
   }
 
 
-  public static void generateCoolNumbers(String regionCode) {
+  public static void generateCoolNumbers() {
 
-    String zero = "0";
-    int parseIntRegion = Integer.parseInt(regionCode);
-    if (parseIntRegion < 10) {
+    for (int region = 1; region <= 199; region++) {
 
-      String temp = regionCode;
+      String regionCode;
+
+      if (region <= 9) {
+      String zero = "0";
+      String temp = String.valueOf(region);
       regionCode = zero;
-      regionCode += zero + temp;
-
-    }
+       regionCode += zero + temp;
+    }else { regionCode = String.valueOf(region);   }
 
     for (int i = 111; i <= 999; i += 111) {
 
@@ -60,13 +68,9 @@ public class CoolNumbers {
 
           for (int h = 0; h < lettersLicensePlates.length; h++) {
 
-           /* System.out.printf("%s%03d%s%02d\n", lettersLicensePlates[g], i,
-                lettersLicensePlates[q] + lettersLicensePlates[h], regionCode);*/
-
             String regionNumber = lettersLicensePlates[g] + i +
                 lettersLicensePlates[q] + lettersLicensePlates[h] + regionCode;
 
-            //   System.out.println( num );
             COLECTION_NUMBER_LIST.add(regionNumber);
             COLECTION_NUMBER_SET.add(regionNumber);
             COLECTION_NUMBER_HASH.add(regionNumber);
@@ -77,82 +81,14 @@ public class CoolNumbers {
       }
     }
 
-    System.out.println("База номеров сгенерированна!");
-
-  }
-
-
-  public static void binarySearch(String carNumber,Collection collection) {
-
-    if (carNumber.matches(REG_NUMBER_CAR) && sizeArray(collection)) {
-
-      Collections.sort(COLECTION_NUMBER_LIST);
-      long start = System.nanoTime();
-      int foundNumber = Collections.binarySearch(COLECTION_NUMBER_LIST, carNumber);
-      long finish = System.nanoTime();
-
-      if (foundNumber >= 0) {
-
-        System.out.println(
-            "Номер автомобиля найден!" +  COLECTION_NUMBER_LIST.get(foundNumber)  + " Позиция на строке базы №"
-                + foundNumber);
-
-        System.out.println("Время  успешного поиска в НС - " + (start - finish) + " в МС" + ((start - finish) / 1000));
-
-      } else   {
-        System.out.println("Номер в базе отсутствует!");
-
-        System.out.println("Время не успешного поиска в НС - " + (start - finish) + " в МС" + ((start - finish) / 1000) );
-
-      }
-
 
     }
 
 
   }
 
-  public static void containsSearch(String carNumber, Collection collection) {
 
-
-    if (carNumber.matches(REG_NUMBER_CAR) && sizeArray(collection)) {
-      long start = System.nanoTime();
-      if (COLECTION_NUMBER_LIST.contains(carNumber)) {
-        long finish = System.nanoTime();
-        System.out.println("Номер автомобиля найден!" + carNumber);
-        System.out.println("Время успешного поиска в НС - " + (start - finish) + " в МС"+((start - finish) / 1000) );
-      } else {long finishSecond = System.nanoTime();
-                System.out.println("Номер в базе отсутствует!");
-
-        System.out.println("Время не успешного поиска в НС - " + (start - finishSecond) + " в МС"+((start - finishSecond) / 1000));
-
-      }
-    }
-  }
-
-
-  public static String inputRegion(String regionCode) {
-
-    if (regionCode.matches(REG_ADD_REGION)) {
-
-      if (REGION_NUMBERS.contains(regionCode)) {
-        System.out.println(
-            "База автомобильных номеров с таким кодом региона уже существует.Введите иной код региона!");
-      } else {
-        REGION_NUMBERS.add(regionCode);
-
-        generateCoolNumbers(regionCode);
-
-      }
-
-
-    }
-
-    return regionCode;
-  }
-
-
-  public static String printArray(String print) {
+  public static void printArray(String print) {
 
     if (print.equals("PRINT")) {
 
@@ -165,25 +101,108 @@ public class CoolNumbers {
 
     }
 
-    return print;
+
   }
 
-  public static boolean sizeArray( Collection collection) {
 
+  public static void searchInArrayList(String value, List list) {
 
-
-  boolean isSize;
-
-    if (collection.size() == 0) {
+    long start = System.nanoTime();
+    if (list.contains(value)) {
+      long finish = System.nanoTime();
 
       System.out.println(
-          "База номеров отсутствует! Сгенерируйте базу  введя необходимый код региона!");
+          "Номер автомобиля найден!" + value);
 
-       isSize= false;
-    } else { isSize = true;}
+      System.out.println(
+          " Метод поиска  contains в ArrayList. Время успешного поиска в НС  - " + (finish - start)
+              + " в МС" + (
+              (finish - start) / 1000));
 
-   return isSize;
+    } else {
+      long finishSecond = System.nanoTime();
+      System.out.println("Номер в базе отсутствует!");
+
+      System.out.println(
+          "Метод поиска  contains в ArrayList . Время не успешного поиска в НС - " + (finishSecond
+              - start) + " в МС"
+              + ((finishSecond - start) / 1000));
+
+    }
+
+    Collections.sort(list);
+    long startOne = System.nanoTime();
+    int foundNumber = Collections.binarySearch(list, value);
+    long finish = System.nanoTime();
+    if (foundNumber >= 0) {
+      System.out.println("Номер автомобиля найден!");
+
+      System.out.println(
+          "Метод поиска  binarySearch в ArrayList . Время  успешного поиска в НС - " + (finish
+              - startOne) + " в МС" + (
+              (finish - startOne) / 1000));
+    } else {
+      long finishTwo = System.nanoTime();
+      System.out.println("Номер в базе отсутствует!");
+
+      System.out.println(
+          "Метод поиска  binarySearch в ArrayList.Время не успешного поиска в НС - " + (finishTwo
+              - startOne) + " в МС" + ((finishTwo - startOne)
+              / 1000));
+    }
+
   }
 
+  public static void searchInTreeSet(String value, Set set) {
+
+    long start = System.nanoTime();
+    if (set.contains(value)) {
+      long finish = System.nanoTime();
+      System.out.println(
+          "Номер автомобиля найден!" + value);
+      System.out.println(
+          " Метод поиска  contains в TreeSet . Время успешного поиска в НС  - " + (finish - start)
+              + " в МС" + (
+              (finish - start) / 1000));
+
+    } else {
+      long finishSecond = System.nanoTime();
+      System.out.println("Номер в базе отсутствует!");
+
+      System.out.println(
+          "Метод contains в TreeSet. Время не успешного поиска в НС - " + (finishSecond - start)
+              + " в МС"
+              + ((finishSecond - start) / 1000));
+
+    }
+
+
+  }
+
+
+  public static void searchInHashSet(String value, Set set) {
+
+    long start = System.nanoTime();
+    if (set.contains(value)) {
+      long finish = System.nanoTime();
+      System.out.println(
+          "Номер автомобиля найден!" + value);
+      System.out.println(
+          " Метод поиска  contains в HashSet . Время успешного поиска в НС  - " + (finish - start)
+              + " в МС" + (
+              (finish - start) / 1000));
+    } else {
+      long finishSecond = System.nanoTime();
+      System.out.println("Номер в базе отсутствует!");
+
+      System.out.println(
+          "Метод contains в HashSet. Время не успешного поиска в НС - " + (finishSecond - start)
+              + " в МС"
+              + ((finishSecond - start) / 1000));
+
+    }
+
+
+  }
 
 }
