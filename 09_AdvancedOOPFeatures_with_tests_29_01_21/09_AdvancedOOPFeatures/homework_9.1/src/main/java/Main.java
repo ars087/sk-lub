550190c7cb2d9d0c65d1eb62cf9d1689101eedd7
Arsen Azaryan <ars5885@gmail.com>
2021-06-23 15:46:53 +0400

    Homework.Module 9,assignments[9.1]/corrections No.1

--- 09_AdvancedOOPFeatures_with_tests_29_01_21/09_AdvancedOOPFeatures/homework_9.1/src/main/java/Main.java
+++ 09_AdvancedOOPFeatures_with_tests_29_01_21/09_AdvancedOOPFeatures/homework_9.1/src/main/java/Main.java
@@ -1,4 +1,3 @@
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -8,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-       List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
+        List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
 
         sortBySalaryAndAlphabet(staff);
 
@@ -16,20 +15,14 @@ public class Main {
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
 
-            staff.sort(((o1, o2) -> {
-            if (o1.getSalary().equals(o2.getSalary())) {
-                return o1.getName().compareTo(o2.getName());
-            } else {
-                return o1.getSalary().compareTo(o2.getSalary());
-            }
-        }));
 
-    //    staff.sort(Comparator.comparing(Employee::getSalary).thenComparing(Employee::getName));
+        staff.sort(Comparator.comparing(Employee::getSalary).thenComparing(Employee::getName));
 
-         for ( Employee e: staff  ){
 
-             System.out.println(e);
-         }
+        for (Employee e : staff) {
+
+            System.out.println(e);
+        }
 
     }
 }
